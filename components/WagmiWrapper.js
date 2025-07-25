4c2508bcc1626fdf25609f3fc768ff0357fc88f9
VINU07 <88230429+Kingvinu7@users.noreply.github.com>
2025-07-25 19:20:55 +0530

    Update WagmiWrapper.js

--- components/WagmiWrapper.js
+++ components/WagmiWrapper.js
@@ -1,11 +1,16 @@
 "use client";
-import { WagmiConfig } from 'wagmi'; // ❗️ not WagmiProvider
+import { WagmiProvider } from 'wagmi';
 import { config } from '@/lib/wagmi.config';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export function WagmiWrapper({ children }) {
   return (
-    <WagmiConfig config={config}>
-      {children}
-    </WagmiConfig>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
